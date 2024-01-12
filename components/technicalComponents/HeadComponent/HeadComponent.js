31ed68822b557892dad5b13136d49022864dc45d
GorAkopian2003 <gor.akopian@ugent.be>
2024-01-12 08:46:52 +0100

    Hotjar

--- components/technicalComponents/HeadComponent/HeadComponent.js
+++ components/technicalComponents/HeadComponent/HeadComponent.js
@@ -43,7 +43,7 @@ export default class HeadComponent extends Component {
 				loaded: document.readyState === "complete"
 			});
 		});
-		hotjar.initialize(3822992, 6);
+		hotjar.initialize(3823020, 6);
 	}
 
 	render() {
